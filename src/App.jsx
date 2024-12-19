c5e58d085199f3a5e63b98f05e9fa334e2454d6b
berk-karademir <berk.karademir@hotmail.com>
2024-12-19 19:00:13 +0300

    Added routes and links for Home, Shop, About Us, and Contact Us pages in menu in header, navigating correctly with proper /urls

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import ShopPage from "./pages/ShopPage";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
 import SignUp from "./components/SignUp";
 import LogIn from "./components/LogIn";
 import { ToastContainer } from 'react-toastify';
@@ -10,33 +13,27 @@ function App() {
   return (
     <>
     <ToastContainer
-position="bottom-center"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-pauseOnHover
-theme="light"
-toastStyle={{fontWeight:"600"}}
-/> 
+      position="bottom-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      toastStyle={{fontWeight:"600"}}
+    /> 
     <Switch>
-      <Route
-        exact
-        path="/"
-        component={() => (
-          <>
-            <HomePage />
-          </>
-        )}
-      />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
+      <Route path="/about" component={AboutUs} />
+      <Route path="/contact" component={ContactUs} />
       <Route path="/signup" component={SignUp} />
       <Route path="/login" component={LogIn} />
     </Switch>
     </>
-
   );
 }
 
--- src/layout/NavBarTitles.jsx
+++ src/layout/NavBarTitles.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NavBarTitles() {
   return (
     <div>
       <div className="flex flex-col items-center text-[#737373] py-14 text-[25px] tracking-tight font-[500]">
         <ul className="text-center">
-          <li>Home</li>
-          <li>Shop</li>
-          <li>About</li>
-          <li>Contact</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/shop">Shop</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
         </ul>
       </div>
     </div>
