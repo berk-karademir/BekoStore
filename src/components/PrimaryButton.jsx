36b44188a51f0d764e8136d029948fba102f58dd
berk-karademir <berk.karademir@hotmail.com>
2024-12-19 15:23:29 +0300

    minor fix

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function PrimaryButton({ children }) {
   return (
     <>
-      <button className="w-64 py-3 px-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400">
+      <button className="w-64 py-3 px-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 my-10">
         {children}
       </button>
       </>
--- src/layout/NavBarTitles.jsx
+++ src/layout/NavBarTitles.jsx
@@ -6,8 +6,8 @@ function NavBarTitles() {
       <div className="flex flex-col items-center text-[#737373] py-14 text-[25px] tracking-tight font-[500]">
         <ul className="text-center">
           <li>Home</li>
-          <li>Product</li>
-          <li>Pricing</li>
+          <li>Shop</li>
+          <li>About</li>
           <li>Contact</li>
         </ul>
       </div>
