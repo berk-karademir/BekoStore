babd4bfd4bdc08516b1ce36102ab0de35faa971d
berk-karademir <berk.karademir@hotmail.com>
2024-12-24 17:31:35 +0300

    updated category display and layout, in ShopPage

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,26 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Header from "@/layout/Header";
-import {
-  calculatePagination,
-  handleNext,
-  handlePrevious,
-  validateFetchedCategories,
-  getCurrentItems,
-} from "../utils/pagination";
 import { fetchCategories } from "../services/fetchCategories";
 import { filterCategories, sortItems } from '@/utils/categoryUtils';
+import Footer from "@/layout/Footer";
 
 const ShopPage = () => {
   const [categories, setCategories] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 8;
   const [selectedGender, setSelectedGender] = useState("");
   const [sortOrder, setSortOrder] = useState(""); // Set to empty string by default
 
   useEffect(() => {
     const getCategories = async () => {
       const fetchedCategories = await fetchCategories();
-      if (validateFetchedCategories(fetchedCategories)) {
+      if (Array.isArray(fetchedCategories) && fetchedCategories.every(category => typeof category === 'object')) {
         setCategories(fetchedCategories);
       } else {
         console.error(
@@ -35,87 +27,38 @@ const ShopPage = () => {
   // Filter categories based on selected gender
   const filteredCategories = filterCategories(categories, selectedGender);
 
-  // Pagination calculations
-  const { totalPages } = calculatePagination(
-    currentPage,
-    itemsPerPage,
-    filteredCategories.length
-  );
-  const currentItems = getCurrentItems(filteredCategories, currentPage, itemsPerPage);
-
   // Sorting logic
-  const sortedItems = sortItems(currentItems, sortOrder);
+  const sortedItems = sortItems(filteredCategories, sortOrder);
 
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
+  const topCategories = categories.sort((a, b) => b.rating - a.rating).slice(0, 5);
 
   return (
     <section>
       <Header />
-      <div className="flex flex-col items-center gap-20">
-        <h5>Home {" > "} Shop</h5>
-        <div className="flex justify-center gap-5">
-          <select
-            value={selectedGender}
-            onChange={(e) => setSelectedGender(e.target.value)}
-            className="border rounded max-w-[60%] p-2"
-          >
-            <option value="">Filter by gender</option>
-            <option value="k">Woman</option>
-            <option value="e">Man</option>
-          </select>
-          <select
-            value={sortOrder}
-            onChange={(e) => setSortOrder(e.target.value)}
-            className="border rounded  max-w-[40%] p-2"
-          >
-            <option value="">Sort by rating</option>
-            <option value="asc">Sort by Rating: Ascending</option>
-            <option value="desc">Sort by Rating: Descending</option>
-          </select>
+      <div className="flex flex-col items-center gap-10">
+        <h5>Home &#10095; <span className="text-[#737373]">Shop</span></h5>
+        <div className="flex flex-col items-center">
         </div>
-        <div className="flex flex-col items-center gap-32">
-          <h3>Categories</h3>
-          {sortedItems.map((category, index) => (
-            <div key={index} className="relative">
-              <img
-              className="w-screen h-auto"
-                src={category.img}
-                alt={category.title}
-                
-              />
-              <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white text-center p-1">
-                {category.gender === "k" ? "Kadın" : "Erkek"} {category.title} (
-                {category.rating}⭐)
+        <div className="flex flex-col items-center gap-10">
+          <h3>Top Categories</h3>
+          <div className="flex flex-col gap-20">
+            {topCategories.map((category) => (
+              <div key={category.id} className="relative">
+                <img
+                  className="w-screen h-auto"
+                  src={category.img}
+                  alt={category.title}
+                />
+                <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white text-center p-2 text-[20px]">
+                  {category.gender === "k" ? "Kadın" : "Erkek"} {category.title} (
+                  {category.rating}⭐)
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-        <div className="bg-white shadow-md rounded-lg flex justify-center items-center gap-5 p-4">
-          <button
-            className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 transition duration-200"
-            onClick={() =>
-              handlePageChange(handlePrevious(currentPage, totalPages))
-            }
-            disabled={currentPage === 1}
-          >
-            Previous
-          </button>
-          <span className="text-lg font-medium">
-            Page {currentPage} of {totalPages}
-          </span>
-          <button
-            className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600 transition duration-200"
-            onClick={() =>
-              handlePageChange(handleNext(currentPage, totalPages))
-            }
-            disabled={currentPage === totalPages}
-          >
-            Next
-          </button>
-        </div>
-      </ div>
+      </div>
+      <Footer/>
     </section>
   );
 };
